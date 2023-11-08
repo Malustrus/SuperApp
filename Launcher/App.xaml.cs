f5c432a51ad48125544eee86acaeb93b232a571e
Clément SALOU <clementsalou@hotmail.fr>
2023-11-08 20:34:26 +0100

    Ajoutez des fichiers projet.

---
+++ Launcher/App.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Launcher
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public App() : base() 
+        {
+            //AppDomain.CurrentDomain.AssemblyResolve += CurrentDomain_AssemblyResolve;
+            
+        }
+
+        private Assembly? CurrentDomain_AssemblyResolve(object? sender, ResolveEventArgs args)
+        {
+            //throw new NotImplementedException();
+            return null;
+        }
+
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            var businessAssembly = Assembly.LoadFrom("SuperAppLogic.dll");
+            Type type = businessAssembly.GetType("SuperAppLogic.ViewModels.MainViewModel");
+            ResourceDictionary rd = new ResourceDictionary();
+            rd.Source = new Uri(@"pack://application:,,,/SuperAppLogic;component/Dictionary.xaml");
+            this.Resources.MergedDictionaries.Add(rd);
+            var mainViewModel = Activator.CreateInstance(type);
+            MainWindow = new MainWindow();
+            MainWindow.DataContext = mainViewModel;
+            MainWindow.Show();
+        }
+    }
+}
---
+++ SuperAppLogic/ViewModels/MainViewModel.cs
@@ -0,0 +1,23 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuperAppLogic.ViewModels
+{
+    public class MainViewModel : ObservableRecipient
+    {
+        public ObservableRecipient CurrentViewModel { get; private set; }
+        public FirstViewModel FirstViewModel { get; private set; }
+        public SecondViewModel SecondViewModel { get; private set; }
+
+        public MainViewModel()
+        {
+            FirstViewModel = new FirstViewModel();
+            SecondViewModel = new SecondViewModel();
+            CurrentViewModel = FirstViewModel;
+        }
+    }
+}
